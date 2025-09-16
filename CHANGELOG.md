01a9f1d70bc78ade145f9388d229823a55d42199
Jordan <6494876+jrdnh@users.noreply.github.com>
2025-09-16 13:05:04 -0400

    Expose financial submodule imports directly as top level imports and bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.3a1] - 2025-09-16
+
+### Added
+- Exposed Accrual, Balance, Payment series classes and related utilities in the public API
+
 ## [0.2.2a1] - 2025-09-15
 
 ### Added
--- src/orcaset/financial/__init__.py
+++ src/orcaset/financial/__init__.py
@@ -3,13 +3,14 @@ from .accrual_node import AccrualSeries, AccrualSeriesBase
 from .balance_node import Balance, BalanceSeries, BalanceSeriesBase
 from .payment_node import Payment, PaymentSeries, PaymentSeriesBase
 from .period import Period, merged_periods
-from .yearfrac import YF
+from .yearfrac import YF, YfType
 
 __all__ = [
     "Accrual",
     "AccrualSeries",
     "AccrualSeriesBase",
     "Period",
+    "YfType",
     "YF",
     "merged_periods",
     "Balance",
